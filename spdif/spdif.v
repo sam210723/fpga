495ed276859b786ff5f827023d9ef7c9c7f9920f
Sam <sam210723@hotmail.com>
2020-06-07 23:10:59 +1000

    I/O naming

--- spdif/spdif.v
+++ spdif/spdif.v
@@ -1,16 +1,16 @@
 module spdif (
-  input  CLK,   // S/PDIF Clock
-  output LED    // On-board LED
+  input  clock,   // S/PDIF Clock
+  output led      // On-board LED
 );
 
   reg [23:0] counter;
 
-  always @ (posedge CLK) begin
+  always @ (posedge clock) begin
     counter <= counter + 1;
 
     if (counter > 16e6) counter <= 0;
   end
 
-  assign LED = (counter < 1e6) || (counter > 3e6 && counter < 4e6);
+  assign led = (counter < 1e6) || (counter > 3e6 && counter < 4e6);
 
 endmodule
