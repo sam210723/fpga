ecf04f669ef851e3244e4f351c488d5142bf0933
Sam <sam210723@hotmail.com>
2020-06-07 23:34:07 +1000

    Sub-frame preambles

--- spdif/spdif.v
+++ spdif/spdif.v
@@ -2,4 +2,9 @@ module spdif (
   input  clock    // S/PDIF Clock
 );
 
+  // Sub-frame Preambles
+  localparam preamble_b = 8'b11101000;  // Channel 1 (Left) and Start of Block
+  localparam preamble_m = 8'b11100010;  // channel 1 (Left)
+  localparam preamble_w = 8'b11100100;  // Channel 2 (Right) or higher
+
 endmodule
