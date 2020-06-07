0b11637efedd8339abea85cfba446f2f51f54042
Sam <sam210723@hotmail.com>
2020-06-07 23:00:53 +1000

    Module blink test

--- spdif/top.v
+++ spdif/top.v
@@ -1,9 +1,12 @@
 module top (
-  input CLK,    // 16 MHz Global Clock
+  input  CLK,   // 16 MHz Global Clock
   output LED,   // On-board LED
   output USBPU  // USB Pull-up Resistor
 );
 
   // Disable USB
   assign USBPU = 0;
+
+  // Create S/PDIF core instance
+  spdif core(CLK, LED);
 endmodule
