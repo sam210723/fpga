1c6a1a3329dcd384d34802e3165e83fde3b1e6e6
Sam <sam210723@hotmail.com>
2020-06-08 11:10:09 +1000

    Block, frame and sub-frame status pins

--- spdif/spdif.v
+++ spdif/spdif.v
@@ -1,6 +1,9 @@
 module spdif (
-  input  clock_i,   // S/PDIF clock
-  output spdif_o    // S/PDIF output
+  input  clock_i,     // S/PDIF clock
+  output spdif_o,     // S/PDIF output
+  output block_o,     // Block status output
+  output frame_o,     // Frame status output
+  output subframe_o   // Sub-frame status output
 );
 
   // Sub-frame Preambles
@@ -9,4 +12,7 @@ module spdif (
   localparam preamble_w = 8'b11100100;  // Channel 2 (Right) or higher
 
   assign spdif_o = 0;
+  assign block_o = 0;
+  assign frame_0 = 0;
+  assign subframe_o = 0;
 endmodule
--- spdif/top.v
+++ spdif/top.v
@@ -2,6 +2,9 @@ module top (
   input  CLK,     // 16 MHz global clock
   output LED,     // On-board LED
   output PIN_4,   // Heartbeat pin
+  output PIN_5,   // Block status pin
+  output PIN_6,   // Frame status pin
+  output PIN_7,   // Sub-frame status pin
   output PIN_8,   // S/PDIF pin
   output USBPU    // USB pull-up
 );
@@ -10,7 +13,7 @@ module top (
   assign USBPU = 0;
 
   // Create S/PDIF core instance
-  spdif core(CLK, PIN_8);
+  spdif core(CLK, PIN_8, PIN_7, PIN_6, PIN_5);
 
   // Create Heartbeat module instance
   wire pulse;
