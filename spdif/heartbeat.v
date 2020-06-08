01e1c440711c4c16a029bcdedadb2bdca08dcc5c
Sam <sam210723@hotmail.com>
2020-06-08 11:05:07 +1000

    Module I/O naming

--- spdif/heartbeat.v
+++ spdif/heartbeat.v
@@ -1,13 +1,13 @@
 module heartbeat (
-  input  clock,   // Global clock input (16 MHz)
-  output pulse    // Heartbeat pulse output
+  input  clock_i,   // Global clock input (16 MHz)
+  output pulse_o    // Heartbeat pulse output
 );
 
   // Register to count clock pulses
   reg [23:0] counter;
 
   // Increment counter on clock pulse
-  always @ (posedge clock) begin
+  always @ (posedge clock_i) begin
     counter <= counter + 1;
 
     // Reset counter at 16,000,000
@@ -15,5 +15,5 @@ module heartbeat (
   end
 
   // Pulse pattern
-  assign pulse = (counter < 1e6) || (counter > 3e6 && counter < 4e6);
+  assign pulse_o = (counter < 1e6) || (counter > 3e6 && counter < 4e6);
 endmodule
--- spdif/spdif.v
+++ spdif/spdif.v
@@ -1,11 +1,12 @@
 module spdif (
-  input  clock,   // S/PDIF clock
-  output spdif    // S/PDIF output
+  input  clock_i,   // S/PDIF clock
+  output spdif_o    // S/PDIF output
 );
 
   // Sub-frame Preambles
-  localparam preamble_b = 8'b11101000;  // Channel 1 (Left) and Start of Block
-  localparam preamble_m = 8'b11100010;  // channel 1 (Left)
+  localparam preamble_b = 8'b11101000;  // Channel 1 (Left) and start of block
+  localparam preamble_m = 8'b11100010;  // Channel 1 (Left)
   localparam preamble_w = 8'b11100100;  // Channel 2 (Right) or higher
 
+  assign spdif_o = 0;
 endmodule
