035172dcac1be9f1a8c297eb4c517322b8f2877b
Sam <sam210723@hotmail.com>
2021-01-10 23:13:52 +1000

    Testbench makefile recipe

---
+++ mimas-v2/picosoc/test/tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+
+module tb;
+    parameter PERIOD = 1;
+
+	reg clk;
+    reg [23:0] data = 24'hFF5511;
+
+	sevenseg uut(
+		.clk(clk),
+		.data(data)
+	);
+ 
+	initial begin
+        $dumpfile("test/tb.vcd");
+        $dumpvars(0, tb);
+        clk = 0;
+	end
+
+    always #PERIOD clk = ~clk;
+ 
+    initial begin
+        $monitor("t=%3d clk=%d\n", $time, clk);
+        #1_000_000; // Simulate for 1ms
+        $finish;
+    end
+
+endmodule
