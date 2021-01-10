c6dd22455cf72278ef25ebf5bd7932a258137f9c
Sam <sam210723@hotmail.com>
2021-01-11 09:56:57 +1000

    Fix tb formatting

--- mimas-v2/picosoc/test/tb.v
+++ mimas-v2/picosoc/test/tb.v
@@ -3,22 +3,22 @@
 module tb;
     parameter PERIOD = 1;
 
-	reg clk;
+    reg clk;
     reg [23:0] data = 24'hFF5511;
 
-	sevenseg uut(
-		.clk(clk),
-		.data(data)
-	);
- 
-	initial begin
+    sevenseg uut(
+        .clk(clk),
+        .data(data)
+    );
+
+    initial begin
         $dumpfile("test/tb.vcd");
         $dumpvars(0, tb);
         clk = 0;
-	end
+    end
 
     always #PERIOD clk = ~clk;
- 
+
     initial begin
         $monitor("t=%3d clk=%d\n", $time, clk);
         #1_000_000; // Simulate for 1ms
