fbeb61a2d31d7f5cdabdbbf3662d36957dbdb3ad
Sam <sam210723@hotmail.com>
2021-02-27 22:24:43 +1000

    Move SFP bring-up project

---
+++ mimas-v2/sfp-eth/rtl/pll/nco.v
@@ -0,0 +1,15 @@
+/*
+ *  nco.v
+ *  http://github.com/sam210723/fpga
+ *
+ *  Numerically Controlled Ocillator for a logic Phase-locked Loop.
+ *
+ */
+
+module nco(
+    input clk
+);
+
+    
+
+endmodule
---
+++ mimas-v2/sfp-eth/rtl/tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module tb;
+    initial begin
+        $dumpfile("test/tb.vcd");
+        $dumpvars(0, tb);
+        clk = 0;
+    end
+
+    // 100 MHz clock
+    reg clk;
+    parameter PERIOD = 5;
+    always #PERIOD clk = ~clk;
+
+
+    /************/
+    /**  UUTs  **/
+    /************/
+
+    nco nco(
+        .clk (clk)
+    );
+
+
+    // Simulation duration
+    initial begin
+        #1_000_000;
+        $finish;
+    end
+
+endmodule
+
+
+`include "rtl/pll/nco.v"
