54fbe4fad2b1ac2aaf97c011af2f82c23313431b
Sam <sam210723@hotmail.com>
2021-04-02 23:06:33 +1000

    Move PLL support
    No PLL in LP1K-CM36

--- icesugar-nano/spdif/pll/spdif_pll.py
+++ icesugar-nano/spdif/pll/spdif_pll.py
@@ -9,18 +9,26 @@ class SPDIF(Elaboratable):
         self.ch = ch
         self.sr = sr
         self.out = Signal()
+        self.pll_clk_freq = 6.144e6 * 4
 
     def elaborate(self, platform):
         m = Module()
 
         # Setup PLL
         if not self.sim:
-            clk_freq = platform.default_clk_frequency
+            m.domains.spdif = cd_spdif = ClockDomain(reset_less=False)
+            m.submodules += PLL(
+                platform.default_clk_frequency,
+                self.pll_clk_freq,
+                odomain="spdif"
+            )
+            platform.add_clock_constraint(cd_spdif.clk, self.pll_clk_freq)
 
         # Test counter
-        counter = Signal(25)
-        m.d.sync += counter.eq(counter + 1)
-        m.d.comb += self.out.eq(counter[19])
+        #counter = Signal(25)
+        #m.d.spdif += counter.eq(counter + 1)
+        #m.d.comb += self.out.eq(counter[24])
+        m.d.spdif += self.out.eq(~self.out.bool())
 
         # Assign pins
         if not self.sim:
@@ -30,6 +38,20 @@ class SPDIF(Elaboratable):
             m.d.comb += pin.o.eq(self.out)
 
         return m
+
+
+class PLL(Elaboratable):
+    # https://github.com/GlasgowEmbedded/glasgow/blob/main/software/glasgow/gateware/pll.py
+
+    def __init__(self, f_in, f_out, odomain, idomain="sync"):
+        self.f_in    = float(f_in)
+        self.f_out   = float(f_out)
+        self.odomain = odomain
+        self.idomain = idomain
+
+    def elaborate(self, platform):
+        if hasattr(platform, "get_pll"): return platform.get_pll(self)
+        raise NotImplementedError("No PLL support for platform")
 
 
 # Build or simulate
