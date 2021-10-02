93a7d290133e0bd08eac5faca8ee0160558d547c
Sam <sam210723@hotmail.com>
2021-10-02 18:37:17 +1000

    Move PLL to top module

--- mimas-v2/sfp/rtl/phy/pma/pll.v
+++ mimas-v2/sfp/rtl/phy/pma/pll.v
@@ -2,16 +2,16 @@
 (* CORE_GENERATION_INFO = "pll,clk_wiz_v3_6,{component_name=pll,use_phase_alignment=true,use_min_o_jitter=true,use_max_i_jitter=false,use_dyn_phase_shift=false,use_inclk_switchover=false,use_dyn_reconfig=false,feedback_source=FDBK_AUTO,primtype_sel=PLL_BASE,num_out_clk=1,clkin1_period=10.0,clkin2_period=10.0,use_power_down=false,use_reset=true,use_locked=true,use_inclk_stopped=false,use_status=false,use_freeze=false,use_clk_valid=false,feedback_type=SINGLE,clock_mgr_type=AUTO,manual_override=false}" *)
 
 module pll (
-    input   CLK_IN,
-    output  CLK_PMA,
+    input   clk_in,
+    output  clk_out,
 
-    input   RESET,
-    output  LOCKED
-);
+    input   reset,
+    output  locked
+    );
 
     IBUFG clkin1_buf (
         .O (clkin1),
-        .I (CLK_IN)
+        .I (clk_in)
     );
 
     wire [15:0] do_unused;
@@ -46,8 +46,8 @@ module pll (
         .CLKOUT4                (clkout4_unused),
         .CLKOUT5                (clkout5_unused),
 
-        .LOCKED                 (LOCKED),
-        .RST                    (RESET),
+        .LOCKED                 (locked),
+        .RST                    (reset),
 
         .CLKFBIN                (clkfbout_buf),
         .CLKIN                  (clkin1)
@@ -59,7 +59,7 @@ module pll (
     );
 
     BUFG clkout1_buf (
-        .O   (CLK_PMA),
+        .O   (clk_out),
         .I   (clkout0)
     );
 endmodule
--- mimas-v2/sfp/rtl/top.v
+++ mimas-v2/sfp/rtl/top.v
@@ -1,20 +1,22 @@
 module top(
-    input   CLK_100MHz,
+    input   CLK_100MHz,             // 100 MHz System Clock
 
-    output  [7:0]   LED
     output  [7:0]   LED,            // Status LEDs
     output  [7:0]   SEV_SEG,        // Seven Segment Display
     output  [2:0]   SEV_SEG_EN      // Seven Segment Display Enable
     );
 
+    // Clock Generator (125 MHz)
     wire clk_eth;
-    wire clk_eth_ok;
-    pma pma (
+    wire pll_ok;
+    pll pll_eth (
         .clk_in   (CLK_100MHz),
-        .clk_pma  (clk_eth),
+        .clk_out  (clk_eth),
 
-        .reset    (1'b0),       // Active LOW
-        .locked   (clk_eth_ok)  // Active HIGH
+        .reset    (1'b0),   // Active LOW
+        .locked   (pll_ok)  // Active HIGH
+    );
+
     // Status Display
     reg [7:0] status = 8'h00;
     hex #(
@@ -27,12 +29,11 @@ module top(
         .segments         (SEV_SEG),
         .segments_enable  (SEV_SEG_EN)
     );
-    
-    assign LED[0]   = 1'b1;         // Always ON
-    assign LED[1]   = clk_eth_ok;   // PLL Locked
-    assign LED[7:2] = 6'b000000;
 
+    // Status LEDs
+    assign LED[0]   = pll_ok;       // PLL Lock Indicator
+    assign LED[7:1] = 6'b0000000;
 endmodule
 
-`include "../rtl/phy/pma/pma.v"
+`include "../rtl/phy/pma/pll.v"     // Phase Locked Loop
 `include "../rtl/hex.v"             // Seven Segment Hexadecimal Display
