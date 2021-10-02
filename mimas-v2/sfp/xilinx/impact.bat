705926e59e4b447940ea97ac1fe7c1405ff3962e
Sam <sam210723@hotmail.com>
2021-10-02 14:21:57 +1000

    Move Xilinx scripts and project files

--- mimas-v2/sfp/xilinx/impact.bat
+++ mimas-v2/sfp/xilinx/impact.bat
@@ -1,6 +1,6 @@
 setLog -file "build/impact.log"
 setMode -bs
 setCable -port auto
-loadProjectFile -file "boards/numato-mimas-v2.cdf"
+loadProjectFile -file "xilinx/impact.cdf"
 Program -e -p 1
 quit
