985157446d45e3b528321584d1609446939dcd69
Sam <sam210723@hotmail.com>
2022-10-02 16:03:27 +1000

    Clock divider parameters

--- sim/ltc/clk_div.v
+++ sim/ltc/clk_div.v
@@ -1,20 +1,20 @@
 /**
- * Fractional Clock Divider
+ * 32-bit Fractional Clock Divider
  */
 
 module clk_div(
-    input  wire reset_n,        // Reset Flag
-    input  wire clk_i,          // Input Clock
-    output wire clk_o,          // Output Clock
-
-    input reg [27:0] clk_i_f,   // Input Clock Frequency (Hz)
-    input reg [27:0] clk_o_f    // Output Clock Frequency (Hz)
+    input  wire reset_n,                    // Reset Flag
+    input  wire clk_i,                      // Input Clock
+    output wire clk_o                       // Output Clock
 );
 
-    reg [27:0] div = clk_i_f / (clk_o_f * 2);   // Divisor Value
-    reg [27:0] step = 28'hFFFFFFF / div;        // Increment Value
-    reg [28:0] count;                           // Counter Register
-    reg out = 1'h1;                             // Output Clock Buffer
+    parameter FREQ_I;                       // Input Clock Frequency (Hz)
+    parameter FREQ_O;                       // Output Clock Frequency (Hz)
+
+    reg [31:0] div = FREQ_I / (FREQ_O * 2); // Divisor Value
+    reg [31:0] step = 32'hFFFFFFFF / div;   // Increment Value
+    reg [32:0] count;                       // Counter Register
+    reg out = 1'h1;                         // Output Clock Buffer
 
     always @(posedge clk_i) begin
         if (reset_n) begin
@@ -22,9 +22,9 @@ module clk_div(
             count <= count + step;
 
             // Toggle output clock and reset when counter overflows
-            if (count[28]) begin
+            if (count[32]) begin
                 out <= ~out;
-                count <= 29'h0;
+                count <= 33'h0;
             end
         end
     end
